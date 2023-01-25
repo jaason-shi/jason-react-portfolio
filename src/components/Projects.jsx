a33b6d70f28c97f8c0ccf0bd219d27987dcd7dd9
jaason-shi <jasons8002@gmail.com>
2023-01-25 00:11:46 -0800

    update minor wording changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,7 +43,7 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 text-[#ccd6f6] border-pink-600">
             Projects
           </p>
-          <p className="py-6">Check out some of my recent projects here</p>
+          <p className="py-6">Check out some of my recent projects!</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
