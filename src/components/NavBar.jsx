552e385b957271b6c734965dcfbba9dabfa7a28e
jaason-shi <jasons8002@gmail.com>
2024-05-15 09:01:29 -0700

    remove title case

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,9 +29,6 @@ const NavBar = () => {
 
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
-      <div>
-        <h1 className="text-5xl font-signature ml-2 text-pink-600">Jason</h1>
-      </div>
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
